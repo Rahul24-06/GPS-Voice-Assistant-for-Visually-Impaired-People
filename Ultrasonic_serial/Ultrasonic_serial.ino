2ef500924755f2a696333af598be24c9d0ba4f2d
the lonely programmer <rahulkhanna24.06@gmail.com>
2019-02-19 18:58:28 +0530

    Add files via upload

---
+++ Ultrasonic_serial/Ultrasonic_serial.ino
@@ -0,0 +1,14 @@
+#include <Ultrasonic.h>
+
+Ultrasonic ultrasonic(9,8); // (Trig PIN,Echo PIN)
+
+void setup() {
+  Serial.begin(9600); 
+}
+
+void loop()
+{
+  Serial.print(ultrasonic.Ranging(CM)); // CM or INC
+  Serial.println(" cm" );
+  delay(100);
+}
